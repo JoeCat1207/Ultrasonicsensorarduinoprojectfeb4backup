9619eb6a178c2883d0735ef6237fb69bef970f52
ChilliCat <36356602+JoeCat1207@users.noreply.github.com>
2025-02-07 08:40:33 -0500

    New code

--- Ultrasonic_Sensor_Lesson_Feb4_CSE.ino
+++ Ultrasonic_Sensor_Lesson_Feb4_CSE.ino
@@ -1,11 +1,15 @@
 const int trigPin = 10;
 const int echoPin = 9;
+const int LEDpin1 = 4;
+const int LEDpin2 = 2;
 
 float duration, distance;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(LEDpin1, OUTPUT);
+  pinMode(LEDpin2, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -16,9 +20,16 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  float duration = pulseIn(echoPin, HIGH);
+  float distance = (duration*.0343)/2;
+  if (distance < 10){
+    digitalWrite(LEDpin2, LOW);
+    Serial.println ("Too close!");
+    digitalWrite(LEDpin1, HIGH);
+  } else if (distance > 500) {
+    digitalWrite(LEDpin1, LOW);
+    Serial.println ("Too far!");
+    digitalWrite(LEDpin2, HIGH);
+  }
   delay(100);
 }
